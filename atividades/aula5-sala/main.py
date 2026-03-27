aa5cb11b2c1e68c671655dfa071706eed37c0fb2
julia.franca@usp.br <julia.franca@usp.br>
2026-03-27 19:32:24 -0300

    Terminando exercício aula5-sala

--- atividades/aula5-sala/main.py
+++ atividades/aula5-sala/main.py
@@ -1,19 +1,20 @@
-from fastapi import FastAPI, Request
-from fastapi import Depends, HTTPException, status, Cookie, Response
+from fastapi import FastAPI, Request, Depends, HTTPException, status, Cookie, Response
 from typing import Annotated
 from pydantic import BaseModel
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 class Usuario(BaseModel):
     nome: str
     senha: str
     bio: str
+
+class Login(BaseModel):
+    nome: str
+    senha: str
 
 # Base de dados
 users_db = []
@@ -22,10 +23,11 @@ users_db = []
 async def root(request: Request):
     return templates.TemplateResponse(request, "signin.html")
 
+
 @app.post("/users")
-async def create_user(usuario: Usuario):
-    users_db.append(usuario)
-    return {"message": "Usuário criado com sucesso"}
+async def create_user(request: Request, usuario: Usuario):
+    users_db.append(usuario.dict())
+    return {"usuario": usuario.nome}
 
 @app.get("/login")
 async def login_page(request: Request):
@@ -34,23 +36,30 @@ async def login_page(request: Request):
 
 # 1. Rota para "Logar" (Define o Cookie)
 @app.post("/login")
-def login(username: str, password: str, response: Response):
+def login(usuario: Login, response: Response):
+
     # Buscamos o usuário usando um laço simples
     usuario_encontrado = None
     for u in users_db:
-        if u["nome"] == username:
+        if u["nome"] == usuario.nome:
             usuario_encontrado = u
             break
     
     if not usuario_encontrado:
         raise HTTPException(status_code=404, detail="Usuário não encontrado")
     
+    if (usuario_encontrado["senha"] == usuario.senha):
+
     # O servidor diz ao navegador: "Guarde esse nome no cookie 'session_user'"
-    response.set_cookie(key="session_user", value=username)
-    return {"message": "Logado com sucesso"}
+        response.set_cookie(key="session_user", value=usuario.nome)
+        return {"usuario": usuario.nome}
+    else:
+        raise HTTPException(status_code=401, detail="Senha incorreta")
+
 
 # 2. A Dependência: Lendo o Cookie
 def get_active_user(session_user: Annotated[str | None, Cookie()] = None):
+
     # O FastAPI busca automaticamente um cookie chamado 'session_user'
     if not session_user:
         raise HTTPException(
@@ -69,5 +78,5 @@ def get_active_user(session_user: Annotated[str | None, Cookie()] = None):
 def show_profile(request: Request, user: dict = Depends(get_active_user)):
     return templates.TemplateResponse(
         request=request, 
-        name="profile.html", 
+        name="perfil.html", 
         context={"nome": user["nome"], "bio": user["bio"]})
